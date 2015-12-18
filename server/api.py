b5d4cc458444f6d3959981f715068a3041ff8163
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2015-12-18 21:06:43 +0800

    Add a simple flask server.

---
+++ server/api.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+DEBUG = os.environ.get('BACKEND_DEBUG') == 'on'
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'test_msg': 'hello world'}
+
+api.add_resource(HelloWorld, '/api/hello')
+
+def run():
+    app.run(debug=DEBUG, port=3001)
+
+if __name__ == '__main__':
+    run()
