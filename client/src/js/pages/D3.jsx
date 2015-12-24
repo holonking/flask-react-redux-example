7f55fe8b58576635924cde112d23752c6a487ced
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2015-12-24 10:43:21 +0800

    D3 bar chart, html, no-faux-dom version.

--- client/src/js/pages/D3.jsx
+++ client/src/js/pages/D3.jsx
@@ -6,14 +6,24 @@ import d3 from 'd3';
 
 export default class Docs extends Component {
   componentDidMount() {
-    d3.select('.chart').append('div').html('<h3>Hello from D3</h3>');
+    var data = [4, 8, 15, 16, 23, 42];
+    var x = d3.scale.linear()
+        .domain([0, d3.max(data)])
+        .range([0, 420]);
+
+    d3.select(".D3--chart")
+    .selectAll("div")
+      .data(data)
+    .enter().append("div")
+      .style("width", function(d) { return x(d) + "px"; })
+      .text(function(d) { return d; });
   }
 
   render() {
     return (
       <div>
-        <PageHeader>D3.js test page <small>Subtitle here</small></PageHeader>
-        <div className="chart"></div>
+        <PageHeader>D3.js test page <small>A bar chart</small></PageHeader>
+        <div className="D3--chart"></div>
       </div>
     );
   }
