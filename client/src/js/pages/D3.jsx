038ef2b7521ac4eb8fe4f34ee7925017ce411b19
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2015-12-24 13:26:52 +0800

    D3 bar chart, html version with react-faux-dom.

--- client/src/js/pages/D3.jsx
+++ client/src/js/pages/D3.jsx
@@ -3,27 +3,58 @@ import { PageHeader } from 'react-bootstrap';
 import ReactFauxDOM from 'react-faux-dom';
 import d3 from 'd3';
 
+var log = logger('D3');
 
-export default class Docs extends Component {
-  componentDidMount() {
-    var data = [4, 8, 15, 16, 23, 42];
+
+class HtmlBarChart extends Component {
+  render() {
+    var data = this.props.data;
+    log.debug('data:', data);
+
     var x = d3.scale.linear()
         .domain([0, d3.max(data)])
         .range([0, 420]);
 
-    d3.select(".D3--chart")
-    .selectAll("div")
-      .data(data)
-    .enter().append("div")
-      .style("width", function(d) { return x(d) + "px"; })
+    var node = ReactFauxDOM.createElement('div');
+    var chart = d3.select(node);
+    chart.classed('D3--chart', true);
+
+    log.debug('node:', node);
+    log.debug('chart:', chart);
+
+    var bar = chart.selectAll('div');
+    var barUpdate = bar.data(data);
+    barUpdate.enter().append('div')
+      .style('width', function(d) { return x(d) + "px"; })
       .text(function(d) { return d; });
+
+    log.debug('bar:', bar);
+    log.debug('barUpdate:', barUpdate);
+
+    return node.toReact();
+  }
+}
+
+
+export default class Docs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: [4, 8, 15, 16, 23, 42]};
+  }
+  componentDidMount() {
+    var newData = [5, 8, 25, 16, 13, 35];
+    setTimeout(() => {
+      this.setState({
+        data: newData,
+      });
+    }, 2000);
   }
 
   render() {
     return (
       <div>
         <PageHeader>D3.js test page <small>A bar chart</small></PageHeader>
-        <div className="D3--chart"></div>
+        <HtmlBarChart data={this.state.data} />
       </div>
     );
   }
