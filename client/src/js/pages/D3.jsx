b74a501f1eb685162693c3de30429b19360c4284
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2015-12-24 10:33:27 +0800

    Add D3 hello world, not using react-faux-dom yet.

--- client/src/js/pages/D3.jsx
+++ client/src/js/pages/D3.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { PageHeader } from 'react-bootstrap';
-
+import ReactFauxDOM from 'react-faux-dom';
+import d3 from 'd3';
 
 
 export default class Docs extends Component {
+  componentDidMount() {
+    d3.select('.chart').append('div').html('<h3>Hello from D3</h3>');
+  }
+
   render() {
     return (
-      <div class="container">
-        <PageHeader>D3.js test page <small>Subtext for header</small></PageHeader>
-        <p>An inverted navbar is black instead of gray.</p>
+      <div>
+        <PageHeader>D3.js test page <small>Subtitle here</small></PageHeader>
+        <div className="chart"></div>
       </div>
     );
   }
